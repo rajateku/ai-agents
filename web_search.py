860a7d57186bdbd3b983f7245fb3569919d12cf4
rajateku <rajatekuiitb@gmail.com>
2025-03-19 20:48:30 +0100

    web agent

---
+++ web_search.py
@@ -0,0 +1,16 @@
+from phi.agent import Agent
+from phi.model.openai import OpenAIChat
+from phi.tools.duckduckgo import DuckDuckGo
+from dotenv import load_dotenv
+
+load_dotenv()
+
+web_agent = Agent(
+    name="Web Agent",
+    model=OpenAIChat(id="gpt-4o"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tool_calls=True,
+    markdown=True,
+)
+web_agent.print_response("Give 10 AI companies that I can appy for jobs in Munich", stream=True)
